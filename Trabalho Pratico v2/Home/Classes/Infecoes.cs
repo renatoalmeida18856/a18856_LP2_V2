b92d1913becabc0952b2427efd21dc6a71b47f20
Renato Almeida <renatoa052@gmail.com>
2020-05-22 23:31:39 +0100

    no message

--- Trabalho Pratico v2/Home/Classes/Infecoes.cs
+++ Trabalho Pratico v2/Home/Classes/Infecoes.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,124 @@ namespace Home.Classes
 {
     class Infecoes
     {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sintomas { get; set; }
+
+        // Ligaçao Base de dados
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        // Seleciona todos da tabela infecoes
+        public DataTable Select()
+        {
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable db = new DataTable();
+            try
+            {
+                string sql = "SELECT * FROM infecoes";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(db);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return db;
+        }
+
+        // Inserir infecoes na tabela
+        public bool Insert(Infecoes i)
+        {
+
+            bool isSucess = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                string sql = "INSERT INTO infecoes (nome, sintomas) VALUES(@nome, @sintomas)";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@nome", i.Nome);
+                cmd.Parameters.AddWithValue("@sintomas", i.Sintomas);
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSucess = true;
+                }
+                else
+                {
+                    isSucess = false;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSucess;
+
+        }
+
+        public bool Update(Infecoes i)
+        {
+
+            bool isSucess = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                string sql = "UPDATE infecoes SET nome=@nome, sintomas=@sintomas WHERE id = @id";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@nome", i.Nome);
+                cmd.Parameters.AddWithValue("@sintomas", i.Sintomas);
+                cmd.Parameters.AddWithValue("@id", i.Id);
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSucess = true;
+                }
+                else
+                {
+                    isSucess = false;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSucess;
+
+        }
+
     }
 }

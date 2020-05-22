d326a9aacd750ce13af05ec78f92ae6ee2297297
Renato Almeida <renatoa052@gmail.com>
2020-05-22 23:33:03 +0100

    no message

--- Trabalho Pratico v2/Home/Classes/Regioes.cs
+++ Trabalho Pratico v2/Home/Classes/Regioes.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,124 @@ namespace Home.Classes
 {
     class Regioes
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Populacao { get; set; }
+
+        // Ligaçao Base de dados
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        // Seleciona todos da tabela regioes
+        public DataTable Select()
+        {
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable db = new DataTable();
+            try
+            {
+                string sql = "SELECT * FROM regioes";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(db);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return db;
+        }
+
+        // Inserir regioes na tabela
+        public bool Insert(Regioes r)
+        {
+
+            bool isSucess = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                string sql = "INSERT INTO regioes (nome, populacao) VALUES(@nome, @populacao)";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@nome", r.Nome);
+                cmd.Parameters.AddWithValue("@populacao", r.Populacao);
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSucess = true;
+                }
+                else
+                {
+                    isSucess = false;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSucess;
+
+        }
+
+        public bool Update(Regioes r)
+        {
+
+            bool isSucess = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                string sql = "UPDATE regioes SET nome=@nome, populacao=@populacao WHERE id = @id";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@nome", r.Nome);
+                cmd.Parameters.AddWithValue("@apelido", r.Populacao);
+                cmd.Parameters.AddWithValue("@id", r.Id);
+
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSucess = true;
+                }
+                else
+                {
+                    isSucess = false;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSucess;
+
+        }
+
+
     }
 }
